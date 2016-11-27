172e1af770b0c7937d86226187e72bfc35ae6d91
Unknown <sergii.kolbasin@albumprinter.com>
2016-11-27 13:21:59 +0100

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+echo 'see more: https://www.microsoft.com/net/core#linuxubuntu'
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get update
 
-sudo apt-get -y install dotnet-sharedframework-microsoft.netcore.app-1.0.0
+echo 'find the available packages: apt-cache search dotnet'
+sudo apt-get install -y dotnet-sharedframework-microsoft.netcore.app-1.0.0
 sudo apt-get install -y dotnet-dev-1.0.0-preview2.1-003177
 dotnet --version
