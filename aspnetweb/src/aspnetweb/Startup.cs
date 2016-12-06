382bbb3e230d947fc8758506e7439b3ed8fb9105
skolbasin <sergii.kolbasin@albumprinter.com>
2016-12-06 17:46:54 +0100

    registered Configuration in DI

--- aspnetweb/src/aspnetweb/Startup.cs
+++ aspnetweb/src/aspnetweb/Startup.cs
@@ -28,6 +28,8 @@ namespace aspnetweb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConfigurationRoot>(Configuration);
+
             // Required to use the Options<T> pattern
             services.AddOptions();
             services.Configure<ValuesController.Options>(Configuration.GetSection("aspnetweb"));
